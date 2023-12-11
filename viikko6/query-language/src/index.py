6820ffd8c658425326c6d9e8a5ac0a4270442e5d
mseppi <miikka.seppi123@gmail.com>
2023-12-11 21:02:24 +0200

    do querybuilder

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,56 +1,49 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or
+from matchers import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
+    
+    query = QueryBuilder()
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+
+    matcher = (
+      query
+      .playsIn("NYR")
+      .hasAtLeast(10, "goals")
+      .hasFewerThan(20, "goals")
+      .build()
     )
 
     for player in stats.matches(matcher):
         print(player)
 
     print()
-
-    matcher = And(
-    HasFewerThan(2, "goals"),
-    PlaysIn("NYR")
-)
-
-    for player in stats.matches(matcher):
-        print(player)
-
-    print()
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
-    print()
-    matcher = Or(
-    HasAtLeast(45, "goals"),
-    HasAtLeast(70, "assists")
-)
-    for player in stats.matches(matcher):
-        print(player)
-
-    print()
-
-    matcher = And(
-    HasAtLeast(70, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
-    )
+    query1 = QueryBuilder()
+    query2 = QueryBuilder()
+    m1 = (
+        query1
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
+        .build()
 )
     
+    m2 = (
+        query2
+        .playsIn("EDM")
+        .hasAtLeast(50, "points")
+        .build()
+    )
+    query = QueryBuilder()
+
+    matcher = query.oneOf(m1, m2).build()
+
     for player in stats.matches(matcher):
         print(player)
-
     
 
 
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -59,3 +59,27 @@ class Or:
                 return True
             
         return False
+class QueryBuilder:
+    def __init__(self):
+        self._query = All()
+
+    def build(self):
+        return self._query
+    
+    def playsIn(self, team):
+        self._query = And(self._query, PlaysIn(team))
+        return self
+    
+    def hasAtLeast(self, value, attr):
+        self._query = And(self._query, HasAtLeast(value, attr))
+        return self
+    
+    def hasFewerThan(self, value, attr):
+        self._query = And(self._query, HasFewerThan(value, attr))
+        return self
+    
+    def oneOf(self, *matchers):
+        self._query = Or(*matchers)
+        return self
+
+    
